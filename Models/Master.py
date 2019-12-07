3b044e2b330a8c0f0dca51a9761d804faf0cf281
RohanRSH <58329550+RohanRSH@users.noreply.github.com>
2019-12-07 14:15:56 +0530

    Update Master.py

--- Models/Master.py
+++ Models/Master.py
@@ -7,8 +7,8 @@ class Master:
     def GetBankDetails():
         BankDetails={"BankDetails": DataAccess.GetBankDetails()}
         return BankDetails
-    def RegisterCandidate(BiceId,Salutation,FirstName,MiddleName,LastName,CountryId,StateId,CityID,DoorNoStreet,Pincode,DOB,Mobile,MaximumEducation,EduOthers,ID_Proof,ID_Number,IdOthers,BankId,BankAccountNumber,UserId,CandidateImageName,IdImage,BankImage,HasBank):
-        response=DataAccess.RegisterCandidate(BiceId,Salutation,FirstName,MiddleName,LastName,CountryId,StateId,CityID,DoorNoStreet,Pincode,DOB,Mobile,MaximumEducation,EduOthers,ID_Proof,ID_Number,IdOthers,BankId,BankAccountNumber,UserId,CandidateImageName,IdImage,BankImage,HasBank)
+    def RegisterCandidate(BiceId,Salutation,FirstName,MiddleName,LastName,CountryId,StateId,CityID,DoorNoStreet,Pincode,DOB,Mobile,MaximumEducation,EduOthers,ID_Proof,ID_Number,IdOthers,BankId,BankAccountNumber,UserId,CandidateImageName,IdImage,BankImage,HasBank,FatherName,MotherName,Religion,Caste):
+        response=DataAccess.RegisterCandidate(BiceId,Salutation,FirstName,MiddleName,LastName,CountryId,StateId,CityID,DoorNoStreet,Pincode,DOB,Mobile,MaximumEducation,EduOthers,ID_Proof,ID_Number,IdOthers,BankId,BankAccountNumber,UserId,CandidateImageName,IdImage,BankImage,HasBank,FatherName,MotherName,Religion,Caste)
         return response
     def GetCandidateList(BiceId,UserId,CandidateName):
         response=DataAccess.GetCandidateList(BiceId,UserId,CandidateName)
